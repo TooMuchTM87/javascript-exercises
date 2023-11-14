2c69f96ce4abd0c7df315cb4521b7a7eefaacb96
TooMuchTM87 <armanicheong808@gmail.com>
2023-11-14 00:43:19 -1000

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    newWord = "";
 
+    if (num < 0) {
+    return 'ERROR';
+    } else {
+    for (let i = 0; i < num; i++) {
+        newWord += `${string}`;
+    }
+    return newWord;
+}
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+/*let repeatString = (i=0, .toEqual ="heyheyhey");
+        if (i < 3, i++, i>= 3)
+    return (.toEqual) */
