4f542873eb8eb018a2e87e5a00ea2abb9e52fdc4
TooMuchTM87 <armanicheong808@gmail.com>
2023-11-20 01:30:35 -1000

    .

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+let bigNum = 0;
+let smallNum = 0;
+if ((numOne < 0 || numTwo < 0) || 
+(Number.isInteger(numOne) === false ||
+ Number.isInteger(numTwo) === false)) {
+return "ERROR";
+} else if (numOne > numTwo) {
+    bigNum = numOne;
+    smallNum = numTwo;
+} else {
+    bigNum = numTwo;
+    smallNum = numOne;
+}
 
+let total = 0;
+for (let i = smallNum; i <= bigNum; i++) {
+    total += i;
+}
+return total;
 };
 
 // Do not edit below this line
