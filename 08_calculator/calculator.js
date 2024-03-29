6f1ec6a13a28dd3441a3e6a38dcaa9c6d01a95d5
TooMuchTM87 <armanicheong808@gmail.com>
2024-03-29 01:17:08 -1000

    finished assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+return arr.reduce((multiply, current) => multiply * current);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let y = 1;
+ if (x === 0) {
+  return 1;
+ } else {
+  for (let i = x; i > 0; i--) {
+    y *= i;
+  }
+  return y;
+ }
 };
 
 // Do not edit below this line
