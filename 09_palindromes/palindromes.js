f28016a1d228053c669db1a6c0f7be060f32748b
TooMuchTM87 <armanicheong808@gmail.com>
2024-03-30 02:13:25 -1000

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+   let a = x.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+   let arr = a.split('').reverse().join('');
+if (arr === a) {
+return true;
+} else {
+    return false;
 };
+}
 
 // Do not edit below this line
 module.exports = palindromes;
