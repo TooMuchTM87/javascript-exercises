567253a292ec395966e56936e2a0b01dbe621e60
TooMuchTM87 <armanicheong808@gmail.com>
2024-04-09 01:44:43 -1000

    .

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let a = 1;
+    let b = 1;
+    if (x < 0) {
+        return "OOPS";
+    }else{
+        for (let i = 3; i <= x; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;    
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
+   return person.reduce((a, b) => {
+    const one = age(a.yearOfBirth, a.yearOfDeath);
+    const two = age(b.yearOfBirth, b.yearOfDeath);
+    if (one < two) {
+        return b;
+    } else {
+        return a;
+    };
+   });
+};
 
+const age = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
